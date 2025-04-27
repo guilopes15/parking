2e7759f7c6425f09981d2e36473cd1d19945f71c
guilherme <guilherme-lopes58@hotmail.com>
2025-04-27 12:46:20 -0300

    Criando apps e configurando models

---
+++ vehicles/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from customers.models import Customer
+
+
+class VehicleType(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='Nome'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Descrição'
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name='Criado em'
+    )
+    updated_at = models.DateField(
+        auto_now=True,
+        verbose_name='Atualizado em'
+    )
+
+        
+    class Meta:
+        verbose_name = 'Tipo de Veiculo'
+        verbose_name_plural = 'Tipos de Veiculos'
+
+    
+    def __str__(self):
+        return self.name
+    
+
+class Vehicle(models.Model):
+    vehicle_type = models.ForeignKey(
+        VehicleType, 
+        on_delete=models.PROTECT, 
+        blank=True, 
+        null=True,
+        related_name='vehicle',
+        verbose_name='Tipo do Veiculo'
+    )
+    licence_plate = models.CharField(
+        max_length=10,
+        unique=True,
+        verbose_name='Placa',
+    )
+    brand = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Marca',
+    )
+    model = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Modelo',
+    )
+    color = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Cor',    
+    )
+    owner = models.ForeignKey(
+     Customer,
+     on_delete=models.PROTECT,
+     blank=True,
+     null=True,
+     related_name='vehicles',
+     verbose_name='Proprietario', 
+    )
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name='Criado em'
+    )
+    updated_at = models.DateField(
+        auto_now=True,
+        verbose_name='Atualizado em'
+    )
+    
+    
+    class Meta:
+        verbose_name = 'Veiculo'
+        verbose_name_plural = 'Veiculos'
+
+    
+    def __str__(self):
+        return self.licence_plate
