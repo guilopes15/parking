5f0df738d9b44c6d2bdfabb6b86a0077c330d8ff
guilherme <guilherme-lopes58@hotmail.com>
2025-06-04 13:24:20 -0300

    Trocando db sqlite para postgres

--- parking/settings.py
+++ parking/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'parking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'parking_service',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'parking_db',
+        'PORT': '5432',
     }
 }
 
